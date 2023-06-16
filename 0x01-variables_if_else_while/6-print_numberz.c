179e564928e7ad1c0c433ad036288a3fd507c88f
Migiro-Johans <migironyogora@gmail.com>
2023-06-16 14:47:40 -0700

    The program uses the putchar function to print single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program prints using putchar()
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	putchar(x);
+	putchar('\n');
+
+	return (0);
+}
