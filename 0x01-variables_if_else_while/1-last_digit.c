82b1a95cff0b6491a6cff675f6d4fb619c5925fc
Migiro-Johans <migironyogora@gmail.com>
2023-06-16 12:34:21 -0700

    A program that checks randomly generated values of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	if (j > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, j);
 	if (j == 0)
-		printf("Last digit of %d is %d and is zero\n", n, j);
+		printf("Last digit of %d is %d and is 0\n", n, j);
 	if (j < 6 && j != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 	return (0);
