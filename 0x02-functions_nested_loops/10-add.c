ca54fec85b90ae5c995a17d642a8318f776c79e5
Migiro-Johans <migironyogora@gmail.com>
2023-06-20 16:17:20 -0700

    program that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Add two integer
+ * @b1: The 1st number to be evaluated
+ * @b2: The 2nd number to be evaluated
+ * Return: integer value
+ */
+
+int add(int b1, int b2)
+{
+	return (b1 + b2);
+}
